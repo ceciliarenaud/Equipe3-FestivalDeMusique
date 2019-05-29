828282509164c4171fb929ee44b547e74dfca219
ceciliarenaud <31736218+ceciliarenaud@users.noreply.github.com>
2019-05-29 10:54:20 +0200

    Add files via upload

---
+++ InsertTableRepresentation.sql
@@ -0,0 +1,5 @@
+INSERT INTO Representation values ('1', '2019-09-05', 'Pink Floyd', '1');
+INSERT INTO Representation values ('2', '2019-09-06', 'Trois Cafés Gourmands', '3');
+INSERT INTO Representation values ('3', '2019-09-06', 'Janheration', '4');
+INSERT INTO Representation values ('4', '2019-09-07', 'Dub Inc', '2');
+INSERT INTO Representation values ('1', '2019-09-07', 'Trust', '1');
